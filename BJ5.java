52272594b43dc66c253715de16a67d5643c3bbf1
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-09-13 12:46:19 +0530

    Add files via upload

---
+++ BJ5.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner; 
+public class Main
+{
+    public static void main (String[] args) {
+    String f ="Java Programming";
+    String f2 ="Java programming";
+    String f3 ="Python Programming";
+    boolean r1=f.equals(f2);
+    System.out.println("String first and second are equal:"+r1);
+    boolean r2 =f.equals(f3);
+    System.out.println("String first and third are equal:"+r2);
+}
+}
