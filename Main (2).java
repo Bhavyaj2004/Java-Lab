63dfb2d133074d33a6b45537c3f9284b260440a6
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-09-27 11:55:26 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+//wap to initialize the array and display it.
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+        int a[]={1,2,3,4,5};
+        for(int i: a)
+        System.out.println(i);
+        }
+    }
