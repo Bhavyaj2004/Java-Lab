32ccaea1257e057779c035b6e31443a3dcc45b82
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-09-27 12:11:37 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+//wap to initialize the array and display it.
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args){
+        int a[]={10,30,2,5,6,7};
+        String s[]={"Dog","root","cat","man"};
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(s));
+        Arrays.sort(s);
+    }
+        
+        }
