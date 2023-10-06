c84284ec990e50adea7fcae4170434193c2c3f97
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-10-06 16:32:53 +0530

    Add files via upload

---
+++ MethodOverwriting.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+// to demonstrate method overiding and inheritance.
+*******************************************************************************/
+class Animal{
+    public void init(){
+    System.out.println("I am an animal");   
+   
+
+}
+}
+class Dog extends Animal
+{
+    public void init(){
+        System.out.println("I am a dog");
+}
+public void bark(){
+    System.out.println("bark!");
+    }
+}
+public class  MethodOverwriting{
+    public static void main(String [] args){
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Animal c=new Dog();
+        animal.init();
+        dog.init();
+        c.init();
+    }
+}
+
+
+
+
+    
+       
+
