d9fcb72c3ced1fe7202e9aab8ece7c7721bde761
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-10-06 16:08:32 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Animal{
+    protected void display()
+{
+    System.out.println("I am an animal");
+}
+}
+class Dog extends Animal {
+    public static void main(String [] args) {
+        Dog  dog=new Dog();
+        dog.display();
+    }
+}
+
