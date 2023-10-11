36927debaec563999971a4602523841715b2372f
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-10-11 12:30:29 +0530

    Add files via upload

--- Main (5).java
+++ Main (5).java
@@ -5,18 +5,12 @@
 Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
-//wap to initialize the array and display it.
-import java.util.*;
-public class Main
-{
-    public static void main(String[] args){
-        int a[]={10,30,2,5,6,7};
-        String s[]={"Dog","root","cat","man"};
-        System.out.println(Arrays.toString(a));
-        Arrays.sort(a);
-        System.out.println(Arrays.toString(a));
-        System.out.println(Arrays.toString(s));
-        Arrays.sort(s);
-    }
-        
-        }
+class Main{
+        final int AGE=32;
+}
+        class A extends  Main{
+            public static void main(String []args){
+            Main m=new Main();
+            m.AGE=35;
+}
+}
