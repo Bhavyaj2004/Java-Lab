24f89de717067530eb0086582ff93e9fc490e213
Bhavyaj2004 <143725856+Bhavyaj2004@users.noreply.github.com>
2023-11-29 11:57:31 +0530

    Add files via upload

--- Main (5).java
+++ Main (5).java
@@ -1,16 +1,10 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+import java.util.*;
 class Main{
-        final int AGE=32;
-}
-        class A extends  Main{
-            public static void main(String []args){
-            Main m=new Main();
-            m.AGE=35;
-}
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Sum will be"+(a+b));
+    }
 }
